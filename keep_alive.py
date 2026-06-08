7f4a26ac4961004626d283238ba8aee8471a7024
B2CLOUDBOT <b2gpt1726@gmail.com>
2026-06-08 13:53:34 +0530

    Create keep_alive.py

---
+++ keep_alive.py
@@ -0,0 +1,14 @@
+from aiohttp import web
+import os
+
+async def handle(request):
+    return web.Response(text="Bot is alive!")
+
+async def start_server():
+    app = web.Application()
+    app.router.add_get("/", handle)
+    runner = web.AppRunner(app)
+    await runner.setup()
+    port = int(os.environ.get("PORT", 8080))
+    site = web.TCPSite(runner, "0.0.0.0", port)
+    await site.start()
